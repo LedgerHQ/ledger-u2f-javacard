6702bcf94576be47ee8cfbef6f9c5687a1d993f0
J08nY <johny@neuromancer.sk>
2018-03-08 18:54:26 +0100

    Add U2F tests of uploading the attestation certificate.

--- src/test/java/com/ledger/u2f/SimulatorTestBase.java
+++ src/test/java/com/ledger/u2f/SimulatorTestBase.java
@@ -8,23 +8,22 @@ import org.junit.Before;
 import org.junit.BeforeClass;
 
 public class SimulatorTestBase {
-    JavaxSmartCardInterface sim;
-    AID aid;
-
+    static JavaxSmartCardInterface sim;
     static final byte[] AIDArray = {(byte) 0xa0, (byte) 0x00, (byte) 0x00, (byte) 0x06, (byte) 0x17, (byte) 0x00, (byte) 0x4f, (byte) 0x97, (byte) 0xa2, (byte) 0xe9, (byte) 0x49, (byte) 0x01};
+    static final AID aid = new AID(AIDArray, (short) 0, (byte) AIDArray.length);
 
     @BeforeClass
     public static void setUpClass() {
-
+        sim = new JavaxSmartCardInterface();
     }
 
     @Before
     public void setUp() {
-        sim = new JavaxSmartCardInterface();
+
     }
 
     public void prepareApplet(byte[] installData) {
-        aid = new AID(AIDArray, (short) 0, (byte) AIDArray.length);
+        // Setup the GlobalPlatform install data format.
         byte[] fullData = new byte[2 + AIDArray.length + 1 + installData.length + 1];
         int offset = 0;
         fullData[offset++] = (byte) AIDArray.length;
@@ -33,18 +32,27 @@ public class SimulatorTestBase {
         fullData[offset++] = 0;
         fullData[offset++] = (byte) installData.length;
         System.arraycopy(installData, 0, fullData, offset, installData.length);
-        aid = sim.installApplet(aid, U2FApplet.class, fullData, (short) 0, (byte) fullData.length);
+
+        sim.installApplet(aid, U2FApplet.class, fullData, (short) 0, (byte) fullData.length);
         sim.selectApplet(aid);
+    }
+
+    public void prepareApplet(byte flags, int attestationCertLength, byte[] attestationPrivKey) {
+        byte[] installData = new byte[35];
+        installData[0] = flags;
+        installData[1] = (byte) ((attestationCertLength & 0xff00) >> 8);
+        installData[2] = (byte) (attestationCertLength & 0xff);
+        System.arraycopy(attestationPrivKey, 0, installData, 3, 32);
+
+        prepareApplet(installData);
     }
 
     @After
     public void tearDown() {
-        sim.reset();
         sim.resetRuntime();
     }
 
     @AfterClass
     public static void tearDownClass() {
-
     }
 }
