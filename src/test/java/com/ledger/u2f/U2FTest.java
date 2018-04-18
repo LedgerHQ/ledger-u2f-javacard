662e24c87d6548d9707acbe4a9010d40000ce6d6
Pavel Břoušek <webmaster@melanger.cz>
2018-04-18 14:39:23 +0200

    tests for compareContantTime

--- src/test/java/com/ledger/u2f/U2FTest.java
+++ src/test/java/com/ledger/u2f/U2FTest.java
@@ -274,4 +274,27 @@ public class U2FTest extends SimulatorTestBase {
         ResponseAPDU getDataAPDU = sim.transmitCommand(new CommandAPDU(0x00, 0xC0, 0, 0));
         assertThat(getDataAPDU.getSW(), is(ISO7816.SW_CONDITIONS_OF_USE_NOT_SATISFIED));
     }
+    
+    @Test
+    public void testCompareConstantTimeEmpty() {
+        byte[] array1 = new byte[]{};
+        byte[] array2 = new byte[]{0x01, 0x01, 0x01};
+        short zero = (short)0;
+        assertThat(FIDOUtils.compareConstantTime(array1, zero, array2, zero, zero), is(false));
+    }
+    
+    @Test
+    public void testCompareConstantTimeNonEqual() {
+        byte[] array1 = new byte[]{0x01, 0x02, 0x01};
+        byte[] array2 = new byte[]{0x01, 0x01, 0x01};
+        short zero = (short)0;
+        assertThat(FIDOUtils.compareConstantTime(array1, zero, array2, zero, (short)3), is(false));
+    }
+    
+    @Test
+    public void testCompareConstantTimeEqual() {
+        byte[] array1 = new byte[]{0x0F, 0x01, 0x02, 0x03, 0x0F};
+        byte[] array2 = new byte[]{0x00, 0x00, 0x00, 0x01, 0x02, 0x03, 0x00, 0x00};
+        assertThat(FIDOUtils.compareConstantTime(array1, (short)1, array2, (short)3, (short)3), is(true));
+    }
 }
